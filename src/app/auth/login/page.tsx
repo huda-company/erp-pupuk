af15e9864839421fbe5268fb67e0a425a338cd79
huda-digitsense <114890923+huda-digitsense@users.noreply.github.com>
2023-09-03 21:44:51 +0700

    layouting: login, dashboard page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+import Image from "next/image";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
+
+export default function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-96">
+        <h1 className="text-2xl font-semibold mb-4">Login</h1>
+        {/* Your form elements go here */}
+        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+          <div className="mb-4">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <Input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              id="email"
+              type="email"
+              placeholder="Email"
+              crossOrigin={undefined}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <Input
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              id="password"
+              type="password"
+              placeholder="******************"
+              crossOrigin={undefined}
+            />
+            <p className="text-red text-xs italic">Please choose a password.</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
